0c280b97caade5a69bffdd1d2c8957fd046eb81b
Chris <chris.gr.skinner@gmail.com>
2022-02-02 08:42:03 +0000

    more functions, more errors select all and select added to artist

--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -11,21 +11,26 @@ def new_artist(artist):
     artist.id = id
     return artist
 
-def delete_all():
+def delete_all_artists():
     sql = "DELETE FROM artists"
     run_sql(sql)
 
-# need select function above this line
-# def find_artist(id):
-#     artist = None
-#     sql = "SELECT * FROM tasks WHERE id = %s"
-#     values = [id]
-#     result = run_sql(sql, values)[0]
+def select_all_artists():
+    artists = []
 
-#     if result is not None:
-#         artist = artist_repository.select(result['artist_id'])
-#         artist = Artist(
-#             result['name'], 
-#             result['id']
-#             )
-#     return artist
+    sql = "SELECT * FROM artists"
+    results = run_sql(sql)
+    for row in results:
+        artist = Artist(row['name'], row['id]'])
+        artists.append(artist)
+    return artists
+
+def select(id):
+    artist = None
+    sql = "SELECT * FROM artists WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        user = Artist(result['name'], result['id'] )
+    return user
